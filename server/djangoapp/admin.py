f1124c19115c0de5016214a54d755c2a8cf4d379
ammarasifDSU <ammarasif9@gmail.com>
2023-04-13 09:37:43 -0400

    Models work

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
+admin.site.register(CarMake)
+
+admin.site.register(CarModel)
+
+
 
 # CarModelInline class
 
-# CarModelAdmin class
+class CarModelInline:
+    pass
 
-# CarMakeAdmin class with CarModelInline
 
 # Register models here
